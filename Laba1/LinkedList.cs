98b36f4ba1376e153a4b641c5aa269648bad1e0e
Ilya-kh <illia.khaminov@nure.ua>
2021-03-07 01:55:58 +0200

    Create class LinkedList

---
+++ Laba1/LinkedList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laba1
+{
+    class LinkedList<T> where T : IComparable
+    {
+        Node<T> first;
+        Node<T> last;
+        int count = 0;
+    }
+}
