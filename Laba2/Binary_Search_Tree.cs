1c99453cfc73e85b28ea72c0568985ec5fbe0138
Ilya-kh <illia.khaminov@nure.ua>
2021-04-05 10:38:21 +0300

    Created class Binary Search Tree

---
+++ Laba2/Binary_Search_Tree.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laba2
+{
+    public class Tree<T> where T : IComparable<T>
+    {
+        public Node<T> head { get; set; }
+        public int count = 0;
+    }
+}
