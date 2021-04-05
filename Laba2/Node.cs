886bcc762226935e584c445cd4e395c5d8347e07
Ilya-kh <illia.khaminov@nure.ua>
2021-04-05 10:36:01 +0300

    Created class Node

---
+++ Laba2/Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laba2
+{
+    public class Node<T> where T : IComparable<T>
+    {
+        public Node<T> left { get; set; }
+        public Node<T> right { get; set; }
+        public Node(T data)
+        {
+            Data = data;
+        }
+        public T Data;
+    }
+}
