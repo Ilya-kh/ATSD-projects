f612bfea0e8605e5fe335ff9796572f63799a1b7
Ilya-kh <illia.khaminov@nure.ua>
2021-05-21 18:30:51 +0300

    function, which add elements, was added

--- Laba3/Heap.cs
+++ Laba3/Heap.cs
@@ -1,7 +1,28 @@
-﻿namespace Laba3
+﻿using System.Collections.Generic;
+
+namespace Laba3
 {
     public class Heap
     {
-        
+        private List<int> _list = new List<int>();
+        private int HeapSize = 0;
+
+        public void Add(int item)
+        {
+            _list.Add(item);
+            HeapSize++;
+            int current = HeapSize - 1;
+            int parent = (current - 1) / 2;
+
+            while (current > 0 && _list[parent] > _list[current])
+            {
+                int temporary = _list[parent];
+                _list[parent] = _list[current];
+                _list[current] = temporary;
+
+                current = parent;
+                parent = (current - 1) / 2;
+            }
+        }
     }
 }
