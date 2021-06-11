7af6c66be5b85f9ba846177f3bb7135fdd241b16
Ilya-kh <illia.khaminov@nure.ua>
2021-06-11 22:29:01 +0300

    Added Heapify, HeapSort, Delete, FindKMax functious

--- Laba3/Program.cs
+++ Laba3/Program.cs
@@ -10,6 +10,16 @@ namespace Laba3
     {
         static void Main(string[] args)
         {
+            int[] array = new[] {5,4,3,2,1};
+            Heap newHeap = new Heap("minheap");
+            newHeap.CreateHeap(array);
+            //newHeap.Delete();
+            //newHeap.Add(10);
+            newHeap.Print();
+            Console.WriteLine();
+            Console.WriteLine(newHeap.isEmpty());
+            Console.WriteLine(newHeap.Size());
+            //newHeap.Find_k_max(2);
         }
     }
 }
